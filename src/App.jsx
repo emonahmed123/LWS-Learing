4aa4410d3d70053774ab8b342a6e66eeb622dd93
Emon Ahmed <imonshomon@gmail.com>
2025-05-12 08:18:18 +0600

    Passing event handlers as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import "./App.css";
 
-function AlertButton({ message, children }) {
-  return <button onClick={() => alert(message)}>{children}</button>;
+function Button({ onClick, children }) {
+  return <button onClick={onClick}>{children}</button>;
 }
 
-export default function App() {
+function PlayButton({ movieName }) {
+  function handlePlayClick() {
+    alert(`Playing ${movieName}!`);
+  }
+
+  return <Button onClick={handlePlayClick}>Play "{movieName}"</Button>;
+}
+
+function UploadButton() {
+  return <Button onClick={() => alert("Uploading!")}>Upload Image</Button>;
+}
+
+export default function Toolbar() {
   return (
     <div>
-      <AlertButton message="Playing!">Play Movie</AlertButton>
-      <AlertButton message="Uploading!">Upload Image</AlertButton>
+      <PlayButton movieName="Kiki's Delivery Service" />
+      <UploadButton />
     </div>
   );
 }
