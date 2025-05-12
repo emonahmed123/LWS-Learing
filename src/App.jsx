4daf548a1b0390a42043b790abbfbd14fef37eee
Emon Ahmed <imonshomon@gmail.com>
2025-05-12 08:09:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function AlertButton({ message, children }) {
+  return <button onClick={() => alert(message)}>{children}</button>;
+}
+
+export default function App() {
+  return (
+    <div>
+      <AlertButton message="Playing!">Play Movie</AlertButton>
+      <AlertButton message="Uploading!">Upload Image</AlertButton>
+    </div>
+  );
+}
