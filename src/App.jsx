41efb43e5e424ecc78e3fbc2494a319414e33f4f
Emon Ahmed <imonshomon@gmail.com>
2025-05-25 10:34:46 +0600

    create simple Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,30 @@
-import { useReducer } from "react";
 import "./App.css";
-import AddTask from "./Componentes/AddTask";
-import TaskList from "./Componentes/TaskList";
-import taskReducer from "./Reducers/taskReducer";
-import { initialTasks } from "./data/task";
+import Heading from "./Componentes/Heading";
+import Section from "./Componentes/Section";
 /*   
-1.Learn Reducer how to work and  how to  comabained to useSate to userRecuder 
-2.Learn how to use dispatch what is dispatch and learn swtich case useses
 
 
 */
 
 export default function App() {
-  const [tasks, dispatch] = useReducer(taskReducer, initialTasks);
-
-  //handle
-
-  const getNextId = (data) => {
-    const maxId = data.reduce(
-      (prev, current) => (prev.id > current.id ? prev.id : current.id),
-      0
-    );
-    return maxId + 1;
-  };
-
-  const handleAddTask = (text) => {
-    dispatch({
-      type: "added",
-      text,
-      id: getNextId(tasks),
-    });
-  };
-
-  const handleChangeTask = (task) => {
-    dispatch({
-      type: "changed",
-      task,
-    });
-  };
-
-  const handleDeleTask = (taskId) => {
-    dispatch({
-      type: "deleted",
-      id: taskId,
-    });
-  };
-
   return (
-    <>
-      <h1>Prague itinerary</h1>
-      <AddTask onAdd={handleAddTask} />
-
-      <TaskList
-        tasks={tasks}
-        onChangeTask={handleChangeTask}
-        onDeletedTask={handleDeleTask}
-      />
-    </>
+    <Section level={1}>
+      <Heading>Title</Heading>
+      <Section level={2}>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section level={3}>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section level={4}>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
   );
 }
---
+++ src/Context/levelContext.js
@@ -0,0 +1,9 @@
+import { createContext } from "react"
+
+
+
+export const LevelContext = createContext(1)
+
+
+
+
