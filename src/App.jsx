ea352d1101963b0c79fc3d2faea49fea76b79aa2
Emon Ahmed <imonshomon@gmail.com>
2025-05-27 08:44:06 +0600

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,69 @@
+import { useState } from "react";
+
 import "./App.css";
-import Heading from "./Componentes/Heading";
-import Section from "./Componentes/Section";
+import AddTask from "./Componentes/AddTask";
+import TaskList from "./Componentes/TaskList";
+
+import { initialTasks } from "./data/task";
+
 /*   
+1.Learn Reducer how to work and  how to  comabained to useSate to userRecuder 
+
 
 
 */
 
 export default function App() {
+  const [tasks, setTask] = useState(initialTasks);
+
+  //handle
+
+  const getNextId = (data) => {
+    const maxId = data.reduce(
+      (prev, current) => (prev.id > current.id ? prev.id : current.id),
+      0
+    );
+    return maxId + 1;
+  };
+
+  const handleAddTask = (text) => {
+    setTask([
+      ...tasks,
+      {
+        id: getNextId(tasks),
+
+        text: text,
+        done: false,
+      },
+    ]);
+  };
+
+  const handleChangeTask = (task) => {
+    const nextTasks = tasks.map((t) => {
+      if (t.id === task.id) {
+        return task;
+      } else {
+        return t;
+      }
+    });
+
+    setTask(nextTasks);
+  };
+
+  const handleDeleTask = (taskId) => {
+    setTask(tasks.filter((t) => t.id !== taskId));
+  };
+
   return (
-   
+    <>
+      <h1>Prague itinerary</h1>
+      <AddTask onAdd={handleAddTask} />
+
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeletedTask={handleDeleTask}
+      />
+    </>
   );
 }
