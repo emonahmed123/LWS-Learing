c8065957cf0de949b2146c633464bde6de096f5b
Emon Ahmed <imonshomon@gmail.com>
2025-06-04 11:56:23 +0600

    done

--- src/components/MyInput.jsx
+++ src/components/MyInput.jsx
@@ -1,7 +1,15 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const MyInput = forwardRef((props, ref) => {
-  return <input {...props} ref={ref} />;
+  const realInputRef = useRef(null);
+  useImperativeHandle(ref, () => ({
+    // Only expose focus and nothing else
+    focus() {
+      realInputRef.current.focus();
+    },
+  }));
+
+  return <input {...props} ref={realInputRef} />;
 });
 
 export default MyInput;
