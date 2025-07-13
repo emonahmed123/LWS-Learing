8c0016929e9fa6dc83b68b31915e88966d37aef4
Emon Ahmed <imonshomon@gmail.com>
2025-07-13 21:16:49 +0600

    done react-hooks from

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import { useState } from "react";
 import "./App.css";
-import AddProduct from "./components/AddProduct";
-import ProductDetails from "./components/ProductDetails";
-import ProductsList from "./components/ProductsList";
-
+import LoginForm from "./forms/LoginForm";
+import RegistrationForm from "./forms/RegistrationForm";
 function App() {
-  const [id, setId] = useState(1);
-
-  const hadleId = (params) => {
-    setId(params);
-  };
-
   return (
     <>
-      <div>
-        <div className="flex">
-          <AddProduct></AddProduct>
-          <h1>API Request with Axios</h1>
-          <ProductsList onhadle={hadleId} />
-          <ProductDetails id={id} />
-        </div>
-      </div>
+      <LoginForm />
+      <RegistrationForm />
     </>
   );
 }
